5ba4f0a2610eb08c93951679ffd5afa4538bc8a5
Mohammed Shafeeq <pkshefi@gmail.com>
2024-10-03 01:15:37 +0530

    ft: scrollview-cutomize-home-draft

--- src/screen/Customize/Customize.jsx
+++ src/screen/Customize/Customize.jsx
@@ -41,6 +41,23 @@ const Customize = () => {
         </div>
 
        
+      <div className="sticky bottom-0 mt-[30px] bg-white w-full overflow-x-auto scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-gray-500 scrollbar-track-gray-300">
+        <div className="flex flex-row gap-[20px] p-[10px]">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <div
+              key={index}
+              className="w-[372.47px] h-[178.05px] flex-shrink-0 bg-white border border-gray-300 rounded-[12px] flex items-center justify-center shadow-lg"
+            >
+              <img
+                src="/assets/Customisation/ScrollCard.png"
+                alt={`Card ${index + 1}`}
+                className="object-cover w-full h-full rounded-[12px]"
+              />
+            </div>
+          ))}
+        </div>
+
+        </div>
       </div>
     </div>
   );
